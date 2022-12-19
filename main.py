036480cd21cd0aee881fad38e0bfc8601026364c
Jan Majchrzak <jan.majchrzak@protonmail.com>
2022-12-19 16:27:50 +0100

    Jebać b*dgoszcz

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-import glob
 from scipy.io import wavfile
 from pylab import *
 from scipy import *
-from pathlib import Path
+import numpy as np
+from os import listdir
+from os.path import isfile, join
 
-male_min_max = [55, 158]
-female_min_max = [170, 295]
-HPSLoop = 6
-samples_number = 3  # Branie pod uwagę tylko 3 1-sekundowych próbek
+MAX_AUDIO_DURATION = 5
+
+MALE_MAX_FREQ = 158
+MALE_MIN_FREQ = 55
+
+FEMALE_MIN_FREQ = 170
+FEMALE_MAX_FREQ = 295
 
 
-def slice_into_samples(samples_counter, data_voice, sample_rate):
-    if samples_counter > len(data_voice) / sample_rate:
-        samples_counter = len(data_voice) / sample_rate
-    data_voice = data_voice[
-                 max(0, int(len(data_voice) / 2) - int(samples_counter / 2 * sample_rate)): min(len(data_voice) - 1,
-                                                                                                int(len(
-                                                                                                    data_voice) / 2) + int(
-                                                                                                    samples_counter / 2 * sample_rate))]
-    # Branie pod uwagę tylko 3 1-sekundowych próbek z analizowanej tablicy
-    part_length = int(sample_rate)
-
-    # Podział tablicy na 3 1-sekundowe próbki
-    # parts1 = [data_voice[i * part_len: (i + 1) * part_len] for i in range(int(samples_counter))]
-    samples_array = [[0 for column in range(part_length)] for row in range(int(samples_counter))]
-    for i in range(int(samples_counter)):
-        samples_array[i] = data_voice[i * part_length: (i + 1) * part_length]
+def slice_audio(data, sample_rate):
+    audio_duration = min(len(data) / sample_rate, MAX_AUDIO_DURATION)
+    samples_array = []
+    for i in range(int(audio_duration)):
+        samples_array.append(data[i * sample_rate: (i + 1) * sample_rate])
     return samples_array
 
 
-def check_gender(result):
-    if sum(result[male_min_max[0]: male_min_max[1]]) > sum(result[female_min_max[0]: female_min_max[1]]):
-        return 0
-    return 1
+def classify(spectrum):
+    male_freq_range = MALE_MAX_FREQ - MALE_MIN_FREQ
+    male_compliance = sum(
+        spectrum[MALE_MIN_FREQ:MALE_MAX_FREQ]) / male_freq_range
+
+    female_freq_range = FEMALE_MAX_FREQ - FEMALE_MIN_FREQ
+    female_compliance = sum(
+        spectrum[FEMALE_MIN_FREQ:FEMALE_MAX_FREQ]) / female_freq_range
+
+    return female_compliance > male_compliance
 
 
 def apply_hanning(data):
@@ -41,79 +40,49 @@ def apply_hanning(data):
     return data
 
 
-def harmonic_product_spectrum(sample_rate, data_voice, samples_array):
+def harmonic_product_spectrum(sample_rate, samples_array):
     result_parts = []
     for data in samples_array:
-        # if len(data) == 0:
-        #    continue
-        # minimalizacja krawędzi przejścia za pomocą okna Hanninga
         windowed_data = apply_hanning(data)
         fft_v = abs(fft.fft(windowed_data)) / sample_rate
-        fft_r = copy(fft_v)
-        # kompresja spektrum
-        for i in range(2, HPSLoop):
-            tab = copy(fft_v[::i])
+        fft_r = fft_v.copy()
+        for i in range(2, 6):
+            tab = fft_v[::i].copy()
             fft_r = fft_r[:len(tab)]
             fft_r *= tab
         result_parts.append(fft_r)
     result = [0] * len(result_parts[int(len(result_parts) / 2)])
     for res in result_parts:
-        #    if len(res) != len(result):
-        #        continue
         result += res
     return result
 
 
-def true_gender(file):
-    # wyodrębnienie oryginalnej płci z nazwy pliku
-    if file.replace("/", "_").replace(".", "_").split("_")[1] == "M":
-        return 0
-    else:
-        return 1
+def get_actual_gender(filename):
+    return "K" in filename
 
 
-def read_files_from_dir(location):
-    files = glob.glob(location)
-    return files
+def read_files_from_dir(path):
+    return [path + f for f in listdir(path) if isfile(join(path, f))]
 
 
-def read_wav(file):
-    sample_rate, data = wavfile.read(file)
-    return sample_rate, data
-
-
-def fill_and_print_coverage_matrix(files):
-    m = [[0, 0], [0, 0]]
+def predict_gender(files):
+    predicted = []
+    actual = []
     for wav in files:
-        sample_rate, data = read_wav(wav)
-        correct_gender = true_gender(wav)  # dla mężczyzny: 0 dla kobiety: 1
-        found_gender = harmonic_product_spectrum(sample_rate, data, slice_into_samples(samples_number, data, sample_rate))
-        m[correct_gender][check_gender(found_gender)] += 1
-    print(m)
-    correctness = (m[0][0] + m[1][1]) / (sum(m[0]) + sum(m[1]))
-    print(correctness)
-
-
-def find_and_print_gender(wav):
-    sample_rate, data = read_wav(wav)
-    found_gender = harmonic_product_spectrum(sample_rate, data, slice_into_samples(samples_number, data, sample_rate))
-    if check_gender(found_gender) == 0:
-        print('M')
-    elif check_gender(found_gender) == 1:
-        print('K')
+        actual.append(get_actual_gender(wav))
+        sample_rate, data = wavfile.read(wav)
+        audio_slices = slice_audio(data, sample_rate)
+        hps_result = harmonic_product_spectrum(sample_rate, audio_slices)
+        predicted.append(classify(hps_result))
+    return predicted, actual
 
 
 if __name__ == "__main__":
-    '''
-    if len(sys.argv) != 2:
-        print("Incorrect number of arguments. Please enter path to one .wav file.")
-        exit(1)
-    path = Path(sys.argv[1])
-    if path.suffix != '.wav':
-        print("Incorrect file extension. Please enter path to .wav file.")
-        exit(1)
-    find_and_print_gender(path)
-    '''
-    path = "trainall/*"
-    files_array = read_files_from_dir(path)
-    fill_and_print_coverage_matrix(files_array)
+    path = "./trainall/"
+    files = read_files_from_dir(path)
+    predicted, actual = predict_gender(files)
+    correct_predictions = 0
+    for i in range(0, len(files)):
+        if predicted[i] == actual[i]:
+            correct_predictions += 1
+    print(correct_predictions / len(files))
